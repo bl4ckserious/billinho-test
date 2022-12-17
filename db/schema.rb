2e872c8ff09cfabfecf17c32263471d4dbcd6f12
Lucca <89747942+bl4ckserious@users.noreply.github.com>
2022-12-17 20:36:39 -0300

    setting rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,57 +1,42 @@
-# frozen_string_literal: true
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_135307) do
+  enable_extension "plpgsql"
 
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema[7.0].define(version: 20_221_216_135_307) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
-
-  create_table 'billings', force: :cascade do |t|
-    t.integer 'enrollment_id'
-    t.float 'amount'
-    t.date 'due_date'
-    t.string 'status'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "billings", force: :cascade do |t|
+    t.integer "enrollment_id"
+    t.float "amount"
+    t.date "due_date"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'enrollments', force: :cascade do |t|
-    t.integer 'student_id'
-    t.integer 'institution_id'
-    t.integer 'amount'
-    t.integer 'installments'
-    t.integer 'due_day'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "institution_id"
+    t.integer "amount"
+    t.integer "installments"
+    t.integer "due_day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'institutions', force: :cascade do |t|
-    t.string 'name'
-    t.string 'cnpj'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "institutions", force: :cascade do |t|
+    t.string "name"
+    t.string "cnpj"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'students', force: :cascade do |t|
-    t.string 'name'
-    t.string 'cpf'
-    t.date 'birth_date'
-    t.string 'payment_method'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "cpf"
+    t.date "birth_date"
+    t.string "payment_method"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'enrollments', 'institutions'
-  add_foreign_key 'enrollments', 'students'
-  add_foreign_key 'billings', 'enrollments'
+  add_foreign_key "enrollments", "institutions"
+  add_foreign_key "enrollments", "students"
+  add_foreign_key "billings", "enrollments"
 end
